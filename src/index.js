31fa43a215e0504a5f61c24264e01a6dda4b9e46
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-10-04 16:56:40 -0300

    Requisito 03 ok

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const path = require('path');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -27,6 +28,11 @@ app.get('/talker/:id', async (req, res) => {
     return res.status(HTTP_OK_STATUS).json(filterById);
   }
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+app.post('/login', (req, res) => {
+    const token = crypto.randomBytes(8).toString('hex');
+    res.status(200).json({ token });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
