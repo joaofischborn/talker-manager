95315c3e801ae4c9dd58b7c597731eaa5c0ad9c2
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-10-04 16:21:09 -0300

    Requisitos 01 e 02 ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
-// Iniciando
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (req, res) => {
+  const data = await fs.readFile(path.resolve(__dirname, './talker.json'), 'utf-8');
+  const talker = JSON.parse(data);
+  if (talker.length > 0) {
+    return res.status(HTTP_OK_STATUS).json(talker);
+  } 
+    return res.status(HTTP_OK_STATUS).json([]);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await fs.readFile(path.resolve(__dirname, './talker.json'), 'utf-8');
+  const talker = JSON.parse(data);
+  const filterById = talker.filter((elem) => elem.id === Number(id));
+  if (filterById.length > 0) {
+    return res.status(HTTP_OK_STATUS).json(filterById);
+  }
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
