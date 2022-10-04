f3cee2cd8614545679629a15c7b6b40af423e838
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-10-04 18:53:48 -0300

    Corrigindo o requisito 02

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ app.get('/talker', async (req, res) => {
 });
 
 app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
+  const idPeople = req.params.id;
   const data = await fs.readFile(path.resolve(__dirname, './talker.json'), 'utf-8');
   const talker = JSON.parse(data);
-  const filterById = talker.filter((elem) => elem.id === Number(id));
-  if (filterById.length > 0) {
+  const filterById = talker.find(({ id }) => id === Number(idPeople));
+  if (filterById) {
     return res.status(HTTP_OK_STATUS).json(filterById);
   }
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
@@ -47,6 +47,29 @@ app.post('/login', (req, res) => {
       return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     } 
       return res.status(HTTP_OK_STATUS).json({ token });
+});
+
+app.post('/talker', (req, res) => {
+  const { name, age, talk, watchedAt, rate } = req.body;
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length < 15) {
+    res.status(401).json({ message: 'Token inválido' });
+  }
+  if (!name) {
+    res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 2) {
+    res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age) {
+    res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  }
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
